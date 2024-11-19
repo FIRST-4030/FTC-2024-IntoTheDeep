3326b6c5f31c162cdfceef119a79457a49633b10
sramorosi <sramorosi@users.noreply.github.com>
2024-11-18 16:35:48 -0800

    added MecanumAutoAlt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MecanumAutoAlt.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.NewMecanumDrive;
+import org.firstinspires.ftc.teamcode.Pose2dWrapper;
+
+@Autonomous
+public class MecanumAutoAlt extends LinearOpMode {
+    NewMecanumDrive drive;
+    IMU imu;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2dWrapper startPose = new Pose2dWrapper(39, -56, Math.toRadians(90));
+        Pose2dWrapper movedPose = new Pose2dWrapper(39, -36, Math.toRadians(90));
+        drive = new NewMecanumDrive(hardwareMap, startPose.toPose2d());
+        imu = hardwareMap.get(IMU.class, "imu");
+
+
+        telemetry.addData("Yaw: ", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+        telemetry.addData("Pitch: ", imu.getRobotYawPitchRollAngles().getPitch(AngleUnit.RADIANS));
+        telemetry.addData("Roll: ", imu.getRobotYawPitchRollAngles().getRoll(AngleUnit.RADIANS));
+        telemetry.update();
+
+        waitForStart();
+
+        Actions.runBlocking(
+                drive.actionBuilder(startPose.toPose2d())
+                        .strafeTo(movedPose.toPose2d().position)
+                        .build()
+        );
+        telemetry.addData("Yaw: ", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+        telemetry.addData("Pitch: ", imu.getRobotYawPitchRollAngles().getPitch(AngleUnit.RADIANS));
+        telemetry.addData("Roll: ", imu.getRobotYawPitchRollAngles().getRoll(AngleUnit.RADIANS));
+        telemetry.update();
+        //Actions.runBlocking(drive.actionBuilder());
+
+    }
+
+}
